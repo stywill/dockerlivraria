c033a354577c21bf0c8854c02e7f8d9643026cc1
stywill <stywill@gmail.com>
2019-02-12 18:21:30 -0200

    testedocker

---
+++ mysql/initial_data/livraria.sql
@@ -0,0 +1,33 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para livraria
+CREATE DATABASE  `livraria` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `livraria`;
+
+DROP TABLE IF EXISTS `books`;
+-- Copiando estrutura para tabela livraria.books
+CREATE TABLE  `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) DEFAULT NULL,
+  `description` text,
+  `isbn` double DEFAULT NULL,
+  `language` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` (`id`, `title`, `description`, `isbn`, `language`) VALUES
+	(1, 'Book title example1', 'Book description example 1', 9781617293290, 'BR'),
+	(2, 'Book title example2', 'Book description example 2', 9781617293291, 'BR'),
+	(3, 'Book title example3', 'Book description example 3', 9781617293292, 'EN'),
+	(4, 'Book title example4', 'Book description example 4', 9781617293293, 'ES');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
